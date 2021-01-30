9fad4daae0f49f986ccd38d40ead1d6f86b8f966
Sheikh Jawwad Mateen <72332780+jawwadmateen@users.noreply.github.com>
2021-01-30 11:37:22 +0500

    Add files via upload

---
+++ exception handling.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+	cout<<"Enter number"<<endl;
+	l1:
+	cin>>x;
+	try
+	{
+		if(x==76)
+		throw x;
+		cout<<"in try"<<endl;
+	}
+	catch(...)
+	{
+		cout<<"nikal";
+		
+	}
+}
